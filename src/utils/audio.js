3a196a8f48730b4004f0ad9ad5d547bbe186a13b
seowxft <62999342+seowxft@users.noreply.github.com>
2021-04-04 19:37:01 +0100

    update

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -2,7 +2,7 @@ const au_play = new Audio("/audio/drop.mp3");
 const au_result = new Audio("/audio/shuffle.mp3");
 
 function result(sound) {
-  let url = `https://github.com/seowxft/memory-yujian/public/audio/${sound}.mp3`;
+  let url = `https://github.com/seowxft/memory-yujian/blob/main/public/audio/${sound}.mp3`;
   if (url !== au_result.src) {
     au_result.src = url;
   }
@@ -11,7 +11,7 @@ function result(sound) {
 }
 
 function play(sound) {
-  let url = `https://github.com/seowxft/memory-yujian/public/audio/${sound}.mp3`;
+  let url = `https://github.com/seowxft/memory-yujian/blob/main/public/audio/${sound}.mp3`;
   if (url !== au_play.src) {
     au_play.src = url;
   }
